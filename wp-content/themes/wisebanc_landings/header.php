289b48cbee2650be77db395306591d24a2a59e0f
Serhiy Hulyi <serhiyhulyi93@gmail.com>
2018-08-27 17:09:45 +0300

    google tag manager

--- wp-content/themes/wisebanc_landings/header.php
+++ wp-content/themes/wisebanc_landings/header.php
@@ -6,6 +6,14 @@
 		<title><?php get_field('display_title') ? the_field('display_title') : wp_title(); ?></title>
 		<meta name="description" content="<?php echo get_post_meta(get_the_ID(), '_yoast_wpseo_metadesc', true); ?>">
 		<meta name="viewport"  content="width=device-width, initial-scale=1">
+
+
+
+		<!-- Google Tag Manager -->
+		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-T3SWLK5');</script>
+		<!-- End Google Tag Manager -->
+
+
 		
 		<?php wp_head(); ?>
 
@@ -37,6 +45,12 @@
 		</script>
 	</head>
 	<body <?php body_class(); ?> >
+
+		<!-- Google Tag Manager (noscript) -->
+		<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T3SWLK5" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+		<!-- End Google Tag Manager (noscript) -->
+
+
 
 		<div class="language-switcher">
 			<?php wb_lang_switcher(); ?>
